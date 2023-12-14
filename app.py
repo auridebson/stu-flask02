b735ce8007c1d3d0f138968ea856c26669de2753
Auridebson Lima <auridebson@vutus.com>
2023-12-14 10:02:39 -0300

    UPdate

--- app.py
+++ app.py
@@ -4,12 +4,18 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
+    d = 3/2
     return 'Index'
 
 @app.route("/contato")
 def contato():
     return 'Contato'
 
+def teste():
+    return '<h3>Testando Flask</h3>'
+
+app.add_url_rule("/teste","teste",teste)
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True, port="3000")
